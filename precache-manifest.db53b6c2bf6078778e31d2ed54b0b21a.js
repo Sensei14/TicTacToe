a4a93493a35044dff517993d07cc746c2b765075
unknown <karox1241@wp.pl>
2019-06-25 16:43:16 +0200

    Updates

---
+++ precache-manifest.db53b6c2bf6078778e31d2ed54b0b21a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80315b1d04877a3be939321b3ce4ce32",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "0f4b958837518b1c8da1",
+    "url": "/TicTacToe/static/css/main.7a99f39f.chunk.css"
+  },
+  {
+    "revision": "275114598dfe6bf12129",
+    "url": "/TicTacToe/static/js/2.9f73500c.chunk.js"
+  },
+  {
+    "revision": "0f4b958837518b1c8da1",
+    "url": "/TicTacToe/static/js/main.a8e7bd7b.chunk.js"
+  },
+  {
+    "revision": "6670768c31502717d35c",
+    "url": "/TicTacToe/static/js/runtime~main.ae000fa4.js"
+  }
+]);
